87324b4d74f426bf16dc09085f3cdc3b5f9c3d25
LAPTOP-78AGCC7N\chris <christopherg@celticchicago.com>
2025-01-25 15:20:54 -0600

    Adjust server function to construct datastore file path dynamically for netlify serverless function.

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,7 +4,11 @@ const path = require("path");
 exports.handler = async (event, context) => {
   try {
     // Load datastore.json from the static files
-    const filePath = path.resolve(__dirname, "../../dist/datastore.json");
+    //const filePath = path.resolve(__dirname, "../../dist/datastore.json");
+
+    const baseDir = process.env.BASE_DIR || __dirname;
+    const filePath = path.resolve(baseDir, "../dist/datastore.json");
+
     const data = fs.readFileSync(filePath, "utf-8");
     return {
       statusCode: 200,
