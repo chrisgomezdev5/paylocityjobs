7b6da550b9a2eca110d7d9c49052ac0c59166761
LAPTOP-78AGCC7N\chris <christopherg@celticchicago.com>
2025-01-25 15:50:22 -0600

    netlify temp debugging

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -6,15 +6,17 @@ exports.handler = async (event, context) => {
     // Load datastore.json from the static files
     //const filePath = path.resolve(__dirname, "../../dist/datastore.json");
 
-    const baseDir = process.env.BASE_DIR || __dirname;
-    const filePath = path.resolve(baseDir, "dist/datastore.json");
+    //const baseDir = process.env.BASE_DIR || __dirname;
+    //const filePath = path.resolve(baseDir, "dist/datastore.json");
 
-    const data = fs.readFileSync(filePath, "utf-8");
+    //const data = fs.readFileSync(filePath, "utf-8");
+
     return {
       statusCode: 200,
-      body: data,
+      body: __dirname,
       headers: {
-        "Content-Type": "application/json",
+        //"Content-Type": "application/json",
+        "Content-Type": "text/html; charset=utf-8",
       },
     };
   } catch (error) {
