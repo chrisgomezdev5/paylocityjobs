c841a76c26440727d963419d9f7d0b5bd6ab80b7
LAPTOP-78AGCC7N\chris <christopherg@celticchicago.com>
2025-01-25 18:05:01 -0600

    Debugging WIP

--- datastore/datastore.js
+++ datastore/datastore.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const importsFolder = path.join(__dirname, '../scraper/imports');
-const outputFilePath = path.join(__dirname, '../netlify/functions/server/data/datastore.json');
+const outputFilePath = path.join(__dirname, '../dist/datastore.json');
 
 (async () => {
   try {
